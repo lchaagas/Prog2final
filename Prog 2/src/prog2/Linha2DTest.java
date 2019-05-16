dd8e1412bdbbe39ae8b23d0c15553593fb69cf3a
lchaagas <50682230+lchaagas@users.noreply.github.com>
2019-05-15 22:22:01 -0300

    Add files via upload

---
+++ Prog 2/src/prog2/Linha2DTest.java
@@ -0,0 +1,15 @@
+package prog2;
+
+import org.junit.Test;
+
+public class Linha2DTest {
+
+	@Test
+	public void LinhaTest() {
+		Linha2D linha = new Linha2D();
+		linha.print();
+	}
+	
+	
+
+}
---
+++ Prog 2/src/prog2/RetanguloTest.java
@@ -0,0 +1,14 @@
+package prog2;
+
+import org.junit.Test;
+
+public class RetanguloTest {
+
+	@Test
+	public void retanguloTest() {
+		Retangulo ret = new Retangulo();
+		ret.print();	
+	}
+
+	
+}
